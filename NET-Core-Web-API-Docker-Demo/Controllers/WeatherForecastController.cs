3d01aeefd6ddf966f6e5a45eca7aad91137338a8
Sunil Agarwal <sunilagwl5@gmail.com>
2024-02-27 14:50:20 +0530

    Update WeatherForecastController.cs

--- NET-Core-Web-API-Docker-Demo/Controllers/WeatherForecastController.cs
+++ NET-Core-Web-API-Docker-Demo/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ namespace NET_Core_Web_API_Docker_Demo.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
