ec3faa1e93cd284e319485d6176b03d773ecb530
Sunil Agarwal <sunilagwl5@gmail.com>
2024-02-27 14:01:40 +0530

    Update WeatherForecast.cs

--- NET-Core-Web-API-Docker-Demo/WeatherForecast.cs
+++ NET-Core-Web-API-Docker-Demo/WeatherForecast.cs
@@ -2,12 +2,18 @@ namespace NET_Core_Web_API_Docker_Demo
 {
     public class WeatherForecast
     {
-        public DateOnly Date { get; set; }
+        public DateOnly Date { 
+            get; set; 
+        }
 
-        public int TemperatureC { get; set; }
+        public int TemperatureC { 
+            get; set; 
+        }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string? Summary { 
+            get; set; 
+        }
     }
 }
