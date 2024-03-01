5ce0e7c399d9fd75f39b828c92ee00595e0d5df6
Sunil Agarwal <sunilagwl5@gmail.com>
2024-03-01 16:10:44 +0530

    Update WeatherForecastController.cs

--- NET-Core-Web-API-Docker-Demo/Controllers/WeatherForecastController.cs
+++ NET-Core-Web-API-Docker-Demo/Controllers/WeatherForecastController.cs
@@ -24,6 +24,7 @@ namespace NET_Core_Web_API_Docker_Demo.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
+            
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
